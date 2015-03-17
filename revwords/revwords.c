ae42fd5bea4c96c2d902d3cea7bab737b189c8bb
Genyaz <genya12321@gmail.com>
2015-03-17 23:59:15 +0300

    Homework 2: clearer behaviour

--- revwords/revwords.c
+++ revwords/revwords.c
@@ -3,34 +3,27 @@
 #define BUFFER_SIZE 4096
 
 char buf[BUFFER_SIZE], prev[BUFFER_SIZE], to_write[BUFFER_SIZE + 1];
-size_t prev_unwritten_start = BUFFER_SIZE;
-size_t prev_unwritten_end = BUFFER_SIZE;
-size_t buf_start = BUFFER_SIZE;
-size_t buf_end = BUFFER_SIZE;
+size_t prev_unwritten_end = 0;
+size_t buf_start = 0;
+size_t buf_end = 0;
 size_t to_write_size = 0;
-int written = 0;
 
-void write_reversed(int end)
+void write_reversed(int end, int space_after)
 {
-    if (written)
-    {
-        to_write_size = 1;
-        to_write[0] = ' ';
-    }
-    else
-    {
-        to_write_size = 0;
-        written = 1;
-    }
+    to_write_size = 0;
     while (buf_start < end)
     {
         to_write[to_write_size++] = buf[--end]; 
     }
-    while (prev_unwritten_start < prev_unwritten_end)
+    while (prev_unwritten_end > 0)
     {
         to_write[to_write_size++] = prev[--prev_unwritten_end];
     }
-    write_(STDOUT_FILENO, to_write, to_write_size);
+    if (space_after)
+    {
+        to_write[to_write_size++] = ' ';
+    }
+    write_(STDOUT_FILENO, to_write, to_write_size);    
 }     
 
 int main()
@@ -45,9 +38,9 @@ int main()
         }
         else if (buf_end == 0)
         {
-            if (prev_unwritten_start < prev_unwritten_end)
+            if (prev_unwritten_end > 0)
             {
-                write_reversed(0);
+                write_reversed(0, 0);
             }
             exit(EXIT_SUCCESS);
         }
@@ -58,19 +51,16 @@ int main()
             {
                 if (buf[i] == ' ')
                 {
-                    write_reversed(i);
+                    write_reversed(i, 1);
                     buf_start = i + 1;                    
                 }
                 i++;
-            }
-            prev_unwritten_start = buf_start;
-            prev_unwritten_end = buf_end;
+            }            
             i = buf_start;
             while (i < buf_end)
             {
-                prev[i] = buf[i];
-                i++;
-            }
+                prev[prev_unwritten_end++] = buf[i++];
+            }            
         }
     }   
 }
