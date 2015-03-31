b1e0a5543e8eaa2e6ec5546c3567a19bd8e4993b
Genyaz <genya12321@gmail.com>
2015-03-31 19:01:12 +0300

    Homework 4: error checking

--- bufcat/bufcat.c
+++ bufcat/bufcat.c
@@ -1,12 +1,16 @@
 #include <bufio.h>
 #include <stdlib.h>
+#include <unistd.h>
 typedef struct buf_t buf_t;
 #define BUF_SIZE 4096
 
 int main()
 {
     buf_t* buf = buf_new(BUF_SIZE);
-    int reading = 1;    
-    while (buf_fill(STDIN_FILENO, buf, BUF_SIZE / 2) > 0 
+    if (buf == NULL) exit(EXIT_FAILURE);
+    int last_read;   
+    while ((last_read = buf_fill(STDIN_FILENO, buf, BUF_SIZE / 2)) > 0 
 	&& buf_flush(STDOUT_FILENO, buf, buf_size(buf)) > 0);
+    if (last_read == 0) exit(EXIT_SUCCESS);
+    exit(EXIT_FAILURE);
 }
